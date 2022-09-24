c996b1d0c9b378b520c9c8d51f056bad8e9ac774
vlaaad <v.proczenko@gmail.com>
2022-09-24 23:16:27 +0200

    Deploy

---
+++ build.clj
@@ -0,0 +1,42 @@
+(ns build
+  (:require [clojure.tools.build.api :as b]
+            [cemerick.pomegranate.aether :as aether]))
+
+(def lib 'io.github.cljfx/dev)
+(def version (format "1.0.%s" (b/git-count-revs nil)))
+(def class-dir "target/classes")
+(def basis (b/create-basis {:project "deps.edn"}))
+(def jar-file (format "target/%s-%s.jar" (name lib) version))
+(def pom-path (format "%s/META-INF/maven/%s/pom.xml" class-dir lib))
+
+(defn deploy [_]
+  (b/delete {:path "target"})
+  (b/write-pom
+    {:basis basis
+     :class-dir class-dir
+     :lib lib
+     :version version
+     :src-dirs ["src"]
+     :scm {:url "https://github.com/cljfx/dev"
+           :tag (b/git-process {:git-args ["rev-parse" "HEAD"]})}})
+  (b/copy-dir
+    {:src-dirs ["src"]
+     :target-dir class-dir})
+  (b/jar {:class-dir class-dir
+          :jar-file jar-file})
+  (aether/deploy
+    :coordinates [lib version]
+    :jar-file jar-file
+    :pom-file pom-path
+    :repository (-> basis
+                    :mvn/repos
+                    (select-keys ["clojars"])
+                    (update "clojars" assoc
+                            :username "vlaaad"
+                            :password (if-let [console (System/console)]
+                                        (-> console
+                                            (.readPassword "Clojars token:" (object-array 0))
+                                            String/valueOf)
+                                        (do (print "Clojars token:")
+                                            (flush)
+                                            (read-line)))))))
